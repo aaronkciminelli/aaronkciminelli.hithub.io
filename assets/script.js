64b9f30d29fe34a2c179d59555ba570afe7bc6dd
Aaron <aaron.k.ciminelli@gmail.com>
2023-02-22 22:19:57 -1000

    AC

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 function loadDoc1() {
-    document.getElementById("myFrame").src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
+    var iframe = document.getElementById("myFrame");
+    iframe.src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
+    iframe.style.display = "block";
   }
   
   function loadDoc2() {
-    document.getElementById("myFrame").src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
-  }
+    var iframe = document.getElementById("myFrame");
+    iframe.src = "https://docs.google.com/document/d/e/2PACX-1vQ4fn4t4f7VpgXXD1jK7
