9e9dc63435438445fd6602f596e1ce6011024bff
Aaron <aaron.k.ciminelli@gmail.com>
2023-02-23 00:16:40 -1000

    adas

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,16 @@ function loadResume(url) {
     document.getElementById('myFrame').src = url;
   }
   
-  const resumeButton = document.getElementById('button1');
-  resumeButton.addEventListener('click', function() {
-    loadResume('assets/Resume.html');
+  document.addEventListener('DOMContentLoaded', function() {
+    const resumeButton = document.getElementById('button1');
+    const portfolioButton = document.getElementById('button2');
+    
+    resumeButton.addEventListener('click', function() {
+      loadResume('assets/Resume.html');
+    });
+    
+    portfolioButton.addEventListener('click', function() {
+      loadPortfolio('assets/Portfolio.html');
+    });
   });
   
-  const portfolioButton = document.getElementById('button2');
-  portfolioButton.addEventListener('click', function() {
-    loadPortfolio('assets/Portfolio.html');
-  });
-
-  function resizeIframe() {
-    const myFrame = document.getElementById('myFrame');
-    myFrame.style.height = myFrame.contentWindow.document.body.scrollHeight + 'px';
-  }
