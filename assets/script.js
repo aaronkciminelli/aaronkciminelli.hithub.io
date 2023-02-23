b6ad67de587b1eee1702d55172e1818c206b960c
Aaron <aaron.k.ciminelli@gmail.com>
2023-02-23 00:14:00 -1000

    sdfwea

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,22 @@
 function loadResume(url) {
     document.getElementById('myFrame').src = url;
-}
+  }
   
-function loadPortfolio(url) {
+  function loadPortfolio(url) {
     document.getElementById('myFrame').src = url;
-}
-
-const resumeButton = document.getElementById('button1');
-resumeButton.addEventListener('click', function() {
+  }
+  
+  const resumeButton = document.getElementById('button1');
+  resumeButton.addEventListener('click', function() {
     loadResume('assets/Resume.html');
-});
+  });
+  
+  const portfolioButton = document.getElementById('button2');
+  portfolioButton.addEventListener('click', function() {
+    loadPortfolio('assets/Portfolio.html');
+  });
+
+  function resizeIframe() {
+    const myFrame = document.getElementById('myFrame');
+    myFrame.style.height = myFrame.contentWindow.document.body.scrollHeight + 'px';
+  }
