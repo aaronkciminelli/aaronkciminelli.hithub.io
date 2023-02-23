426fd5e88751746f74867d812138c2aa5e4377bd
Aaron <aaron.k.ciminelli@gmail.com>
2023-02-22 23:50:32 -1000

    asdasd

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,8 @@
-function loadDoc1() {
-    var iframe = document.getElementById("myFrame");
-    iframe.src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
-    iframe.style.display = "block";
+function loadResume(url) {
+    document.getElementById('myFrame').src = url;
   }
   
-function loadDoc2() {
-    var iframe = document.getElementById("myFrame");
-    iframe.src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
-    iframe.style.display = "block";
-    }
-
-    function resizeIframe() {
-        var iframe = document.getElementById("myFrame");
-        iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-      }
-      
-      window.addEventListener("message", function(event) {
-        if (event.data === "resize") {
-          resizeIframe();
-        }
-      });
-      
+  function loadPortfolio(url) {
+    document.getElementById('myFrame').src = url;
+  }
+  
