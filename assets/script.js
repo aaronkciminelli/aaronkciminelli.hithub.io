567c4281db2b1d4019a4c17143ed37ad847cbb3c
Aaron <aaron.k.ciminelli@gmail.com>
2023-02-22 22:11:40 -1000

    Aaron Ci

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function loadDoc1() {
+    document.getElementById("myFrame").src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
+  }
+  
+  function loadDoc2() {
+    document.getElementById("myFrame").src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
+  }
