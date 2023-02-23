6cf4c649d5569b2623415b91cee6a9af5091ad90
Aaron <aaron.k.ciminelli@gmail.com>
2023-02-23 00:04:12 -1000

    dsafsdf

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
 function loadResume(url) {
     document.getElementById('myFrame').src = url;
-  }
+}
   
-  function loadPortfolio(url) {
+function loadPortfolio(url) {
     document.getElementById('myFrame').src = url;
-  }
-  
+}
+
+const resumeButton = document.getElementById('button1');
+resumeButton.addEventListener('click', function() {
+    loadResume('assets/Resume.html');
+});
