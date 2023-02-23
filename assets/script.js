b1e4d2c18cdf4c17d8a0265b296e29d48948e9cd
Aaron <aaron.k.ciminelli@gmail.com>
2023-02-22 22:22:53 -1000

    AC

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,6 @@ function loadDoc1() {
   
   function loadDoc2() {
     var iframe = document.getElementById("myFrame");
-    iframe.src = "https://docs.google.com/document/d/e/2PACX-1vQ4fn4t4f7VpgXXD1jK7
+    iframe.src = "https://docs.google.com/document/d/e/2PACX-1vS6AoupbmijpMJpz3-WUaqHxIZMeKcawvguc1OKPUILXBUQU8IzzHKCCq-rLTgyYRPoGY09CCz_qdBA/pub?embedded=true";
+    iframe.style.display = "block";
+    }
