e4272a11efc2480b27096bee773ac5319ba85723
Aaron <aaron.k.ciminelli@gmail.com>
2023-03-28 15:29:23 -1000

    acc

--- script/script.js
+++ script/script.js
@@ -1,17 +1,14 @@
 // JavaScript Document
-// Get the current page's URL
-var currentPageUrl = window.location.href;
 
-// Get the link elements in the navigation
-var navLinks = document.querySelectorAll('.nav a');
+const form = document.querySelector('#contact-form');
 
-// Loop through the link elements
-for (var i = 0; i < navLinks.length; i++) {
-  var linkUrl = navLinks[i].href;
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
 
-  // Check if the link's URL matches the current page's URL
-  if (linkUrl === currentPageUrl) {
-    // Add the active class to the link element
-    navLinks[i].classList.add('active');
-  }
-}
+  const name = document.querySelector('#name').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#message').value;
+
+  // perform form validation here
+  // send form data to server using fetch() or XMLHttpRequest()
+});
